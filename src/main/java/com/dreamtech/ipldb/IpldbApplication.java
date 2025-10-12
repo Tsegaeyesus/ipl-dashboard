0f9a37fac45a62ebb6a6608cc55de25098ccba8a
TsegaeyesusGoha <tsegaeyesus.goha@gmail.com>
2025-10-12 17:24:27 +0300

    add starting boiler-plate code

---
+++ src/main/java/com/dreamtech/ipldb/IpldbApplication.java
@@ -0,0 +1,13 @@
+package com.dreamtech.ipldb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpldbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpldbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dreamtech/ipldb/IpldbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamtech.ipldb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpldbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
