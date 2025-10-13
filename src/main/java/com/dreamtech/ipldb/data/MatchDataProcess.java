1648ac050f7f492aa9f79f33c511154b1048c987
Gohatsibah <gohatsibahw@gmail.com>
2025-10-13 17:35:17 +0300

    add Item processor class

--- src/main/java/com/dreamtech/ipldb/data/MatchDataProcess.java
+++ src/main/java/com/dreamtech/ipldb/data/MatchDataProcess.java
@@ -5,6 +5,8 @@ import org.springframework.batch.item.ItemProcessor;
 import org.springframework.cglib.core.Local;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 public class MatchDataProcess implements ItemProcessor<MatchInput,Match> {
     @Override
@@ -15,6 +17,27 @@ public class MatchDataProcess implements ItemProcessor<MatchInput,Match> {
         match.setDate(LocalDateTime.parse(matchInput.getDate()));
         match.setPlayOfMatch(matchInput.getPlayer_of_match());
         match.setVenue(matchInput.getVenue());
+        String firstInningTeam,secondInningTeam="";
+        if(matchInput.getToss_decision().equalsIgnoreCase("bat")){
+          firstInningTeam=matchInput.getToss_winner();
+            secondInningTeam= matchInput.getWinner().equalsIgnoreCase(matchInput.getTeam1()) ?
+                    matchInput.getTeam2() :
+                    matchInput.getTeam1();
+        }
+        else{
+           secondInningTeam= matchInput.getToss_winner();
+            firstInningTeam=matchInput.getWinner().equalsIgnoreCase(matchInput.getTeam1()) ?
+                    matchInput.getTeam1() :
+                    matchInput.getTeam2();
+        }
+        match.setTeam1(firstInningTeam);
+        match.setTeam2(secondInningTeam);
+        match.setTossDecision(matchInput.getToss_winner());
+        match.setTossDecision(matchInput.getToss_decision());
+        match.setResult(matchInput.getResult());
+        match.setResultMargin(matchInput.getResult_margin());
+        match.setUmpire1(matchInput.getUmpire1());
+        match.setUmpire2(matchInput.getUmpire2());
 
 
         return null;
