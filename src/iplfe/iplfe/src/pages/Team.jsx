ef3bb070b711cafece3f704050d682c8fa794a3d
Gohatsibah <gohatsibahw@gmail.com>
2025-11-01 12:53:06 +0300

    modify page to add react router use param and fetch api modification

--- src/iplfe/iplfe/src/pages/Team.jsx
+++ src/iplfe/iplfe/src/pages/Team.jsx
@@ -1,44 +1,66 @@
 import {React,useState,useEffect} from 'react'
+import { useParams } from 'react-router-dom';
 import {MatchSmall} from '../components/MatchSmall';
 import {MatchDetail} from '../components/MatchDetail';
 
 
 
 export const Team=()=>{
+    const {teamName}=useParams();
     const [team,setTeam]=useState({});
     const [match,setMatch]=useState([])
-    const [error,setError]=userState([])
+    const [err,setError]=useState()
 
      useEffect(
 ()=>{
    const fetchTeam= async ()=>{
-        const response=await fetch('http://localhost:9091/api/v1/teams/Delhi Capitals')
-        .then(response=>response.json()
-        .then((data) =>{
-            console.log(data);
-            setTeam(data);
-            console.log(data.matchList)
-            setMatch(data.matchList)
-        })).catch((err)=>setError(err))
+    try{
+        const encodedTeamName = encodeURIComponent(teamName);
+        const url=`http://localhost:9091/api/v1/teams/${encodedTeamName}`;
+// console.log(url)
+      const response=await fetch(url);
+      if(!response.ok){
+    throw new Error("Not found")
+      }
+
+    const data= await response.json();
+    console.log(data);
+    setTeam(data);
+    console.log(data.matchList)
+    setMatch(data.matchList)
+        }
+    catch(error){
+            console.log("error message",error.message);
+            setError(error.message);
+        }
     }
     fetchTeam();
 }
-    ,[]);
+    ,[teamName]);
+        if(Object.keys(team).length === 0 && !err)
+            {
+            return (<h2>Loading...</h2>)
+            }
+
     return (
     
+ 
         <div className='team'>
-            <h2>Team {team.teamName}</h2>
-            <h3>Total Matches: {team.totalMatches} TotalWins: {team.totalWins}</h3>
+      
+            <h2>Team: {err ? <i>{err}</i>: team.teamName}</h2> 
+           
+            {err ? err: <h3>Total Matches: {team.totalMatches} TotalWins: {team.totalWins}</h3> }
+           
             <h4>Latest Match</h4>
     {
-        match==null?err:match.slice(0,1).map((match)=>{
-            return <MatchDetail key={match.id} match={match}/>
+        match && match.length <=0 ? err:match.slice(0,1).map((match)=>{
+            return <MatchDetail key={match.id} teamName={team.teamName} match={match}/>
         })
     }
-           <h5>Previous Matches</h5>
+           <h5>More Matches</h5>
            {
-match==null? err :match.slice(0,4).map((t)=>{
-  return <MatchSmall key={t.id} match={t}/>
+ match && match.length <=0 ? err :match.slice(1,4).map((t)=>{
+  return <MatchSmall key={t.id} teamName={team.teamName} match={t}/>
 })
            }
         </div>
