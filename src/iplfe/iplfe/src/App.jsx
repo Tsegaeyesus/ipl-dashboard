b604dd42190a29f8f0d731c11e9e9d9c685d76c9
Gohatsibah <gohatsibahw@gmail.com>
2025-11-01 12:53:34 +0300

    add react router config

--- src/iplfe/iplfe/src/App.jsx
+++ src/iplfe/iplfe/src/App.jsx
@@ -1,9 +1,15 @@
 import {Team} from './pages/Team'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className='App'>
-  <Team/>
+    <div className='App' >
+      <Router >
+      <Routes>
+        <Route path="/teams/:teamName" element={<Team/>} />
+      </Routes>
+      </Router>
+
     </div>
   )
 
