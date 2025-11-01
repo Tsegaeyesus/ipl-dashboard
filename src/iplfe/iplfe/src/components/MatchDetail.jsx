97b88eeee00e7871b07413afa6ba09055d79fcb2
Gohatsibah <gohatsibahw@gmail.com>
2025-11-01 12:54:12 +0300

    modify to show other team only and link to teams

--- src/iplfe/iplfe/src/components/MatchDetail.jsx
+++ src/iplfe/iplfe/src/components/MatchDetail.jsx
@@ -1,8 +1,17 @@
 import {React} from 'react'
-export const MatchDetail=({match})=>{
+import {Link} from 'react-router-dom'
+export const MatchDetail=({teamName,match})=>{
+       
+        const otherTeam= teamName===match.team1 ? match.team2 :match.team1;
+        const otherTeamRoute=`/teams/${otherTeam}`
     return (
         <div>
-            <p>{match.team1} Vs {match.team2}</p>
+
+            <h3>Vs <Link to={otherTeamRoute}>{otherTeam} </Link> </h3>
+                <p>The match took place on <b>{match.date }</b> in the city of <b>{match.city }</b> 
+                and Stadium <b>{match.venue }</b>
+                and the winner was <b>{match.matchWinner } by {match.resultMargin}</b></p>
+            
         </div>
     )
 }
