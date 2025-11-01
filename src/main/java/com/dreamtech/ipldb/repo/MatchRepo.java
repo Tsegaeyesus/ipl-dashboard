e2f72e9c2c5f0cc864b0addc302f67ee287b04c7
TsegaeyesusGoha <tsegaeyesus.goha@gmail.com>
2025-11-01 16:14:42 +0300

    modify get matches to be case insensetivey

--- src/main/java/com/dreamtech/ipldb/repo/MatchRepo.java
+++ src/main/java/com/dreamtech/ipldb/repo/MatchRepo.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Repository
 public interface MatchRepo extends JpaRepository<Match,Long> {
 
-    @Query(value="select * from Match where (team1=?2 or team2= ?2) and EXTRACT(YEAR from date)=?1",nativeQuery = true)
+    @Query(value="select * from Match where (upper(team1)=upper(?2) or upper(team2)=upper(?2)) and EXTRACT(YEAR from date)=?1",nativeQuery = true)
     List<Match> getMatchesPerYear(int year, String teamName);
     @Query(value="select * from Match where (upper(team1)=upper(?1) or upper(team2)= upper(?1)) order by date desc",nativeQuery = true)
     List<Match> getAllMatchByTeamNameOrderByDateDesc(String teamName, Pageable pageable);
