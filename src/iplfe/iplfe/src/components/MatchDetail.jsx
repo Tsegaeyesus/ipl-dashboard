a70d6ae45254b29ee92913d0a77a61025a955de6
TsegaeyesusGoha <tsegaeyesus.goha@gmail.com>
2025-11-02 19:44:34 +0300

    add Match Detail component css

--- src/iplfe/iplfe/src/components/MatchDetail.jsx
+++ src/iplfe/iplfe/src/components/MatchDetail.jsx
@@ -1,16 +1,32 @@
 import {React} from 'react'
 import {Link} from 'react-router-dom'
+import './MatchDetail.scss'
+
 export const MatchDetail=({teamName,match})=>{
        
         const otherTeam= teamName===match.team1 ? match.team2 :match.team1;
         const otherTeamRoute=`/teams/${otherTeam}`
+        const IsWon=teamName===match.matchWinner
     return (
-        <div>
+        <div className={`MatchDetail ${IsWon ? 'match-detail-won':'match-detail-lose'}`}>
+            {/* <div className={`MatchDetail ${IsWon ? 'match-detail-won' : 'match-detail-lose'}`}> */}
 
-            <h3>Vs <Link to={otherTeamRoute}>{otherTeam} </Link> </h3>
-                <p>The match took place on <b>{match.date }</b> in the city of <b>{match.city }</b> 
-                and Stadium <b>{match.venue }</b>
-                and the winner was <b>{match.matchWinner } by {match.resultMargin}</b></p>
+                
+                <div className='match-dtail-main'>
+                <span>Vs</span> 
+                    <h3 className='match-team'><Link to={otherTeamRoute}>{otherTeam} </Link> </h3>
+                    <h4 className='match-date'>{match.date }</h4>
+                    <h4 className='match-venue'>{match.venue }</h4>
+                    <h4 className='match-winner'><b>{match.matchWinner } Won by {match.resultMargin} wickets</b></h4>
+
+                </div>
+                <div className='match-detail-extra'>
+                <span>First Inning:</span><h4 className='match-firstInning'>{match.team1}</h4>
+                <span>Second Inning:</span><h4 className='match-secondInning'>{match.team2}</h4>
+                <span>Man of the match:</span><h4 className='match-manofzmatch'>{match.playerOfMatch}</h4>
+                <span>Umpires:</span><h4 className='match-umpires'>{match.umpire1} and {match.umpire2}</h4>
+
+                </div>
             
         </div>
     )
--- src/iplfe/iplfe/src/components/MatchSmall.jsx
+++ src/iplfe/iplfe/src/components/MatchSmall.jsx
@@ -1,12 +1,16 @@
 import {React} from 'react'
 import {Link} from 'react-router-dom'
+import './MatchSmall.scss'
+
 export const MatchSmall=({teamName,match})=>{
   const otherTeam=teamName===match.team1?match.team2:match.team1;
   const otherTeamRoute=`/teams/${otherTeam}`;
+  const IsWon=teamName===match.matchWinner
     return (
-        <div>
-      <p>vs <Link to={otherTeamRoute}>{otherTeam}</Link></p>
-      <p><b>{match.matchWinner} wins the match by {match.resultMargin} wickets</b></p>
+         <div className={`Match-small ${IsWon ? 'match-detail-won':'match-detail-lose'}`}>
+      <h4>vs <Link to={otherTeamRoute}>{otherTeam}</Link></h4>
+      <h4>{match.matchWinner} wins the match by {match.resultMargin} wickets</h4>
+   
         </div>
     )
 }
