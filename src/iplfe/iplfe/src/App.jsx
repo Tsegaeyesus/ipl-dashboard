617c2ea3c9c8159ab54a910b0d03bb0f10880068
TsegaeyesusGoha <tsegaeyesus.goha@gmail.com>
2025-11-02 19:49:13 +0300

    add route for team and match pages

--- src/iplfe/iplfe/src/App.jsx
+++ src/iplfe/iplfe/src/App.jsx
@@ -1,13 +1,16 @@
-import {Team} from './pages/Team'
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import { Team } from './pages/Team'
+import { Match } from './pages/Match'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className='App' >
       <Router >
-      <Routes>
-        <Route path="/teams/:teamName" element={<Team/>} />
-      </Routes>
+
+        <Routes>
+            <Route path="/teams/:teamName/matches/:year" element={<Match/>} />
+            <Route path="/teams/:teamName" element={<Team />} />
+        </Routes>
       </Router>
 
     </div>
