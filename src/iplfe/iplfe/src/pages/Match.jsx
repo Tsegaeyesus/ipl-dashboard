e69f7331503e1e2c76e9789c3c472f1da5c86ddd
TsegaeyesusGoha <tsegaeyesus.goha@gmail.com>
2025-11-08 19:04:01 +0300

    add a year selector on the match page

--- src/iplfe/iplfe/src/pages/Match.jsx
+++ src/iplfe/iplfe/src/pages/Match.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react"
 import { useParams } from 'react-router-dom'
 import { MatchDetail } from "../components/MatchDetail"
+import './Match.scss'
+import { YearSelector } from "../components/YearSelector"
 
 export const Match = () => {
 
@@ -9,7 +11,23 @@ export const Match = () => {
     // const teamName=encodeURIComponent()
     const { teamName, year } = useParams()
 
+   const fetchmatchesOnChange=async(year)=>{
+        try{
+        const response=await fetch(`http://localhost:9090/api/v1/matches/${teamName}?year=${year}`)
+        const data=await response.json();
+        setMatch(data)
+        if(!response){
+            throw new Error("no mache found")
+        }
+    }
+    catch(err){
+            console.log(err)
+            setError(err)
+    }
+
+    }
     useEffect(() => {
+    
         const fetchMatches = async () => {
             try {
                 const response = await fetch(`http://localhost:9090/api/v1/matches/${teamName}?year=${year}`)
@@ -26,19 +44,32 @@ export const Match = () => {
             }
         }
         fetchMatches();
-    }, [])
+    }, [year])
 
     if (Object.keys(match) === 0 && !error) {
         <h2>Loading...</h2>
     }
     return (
-        <div>
+        <div className="match-page">
+            <div className="years">
+
+                <YearSelector/>
+
+            
+            </div>
+            <div className="matches">
             <h1>Match Page</h1>
             {
-                match.map((match) => {
-                    return <MatchDetail key={match.id} match={match} teamName={teamName}/>
-                })
+             Object.keys(match).length>0 ?  
+             (
+
+                 match.map((match) => {
+                        return <MatchDetail key={match.id} match={match} teamName={teamName}/>
+                    })
+                
+             ) :<h2>No match found for this year</h2>
             }
+            </div>
         </div>
     )
 }
